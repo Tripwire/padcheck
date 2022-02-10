12f6c4c1b621f329ffd8dd46cd47dd7ea44548da
Hyeon Kim <simnalamburt@gmail.com>
2022-02-10 18:26:22 +0900

    Support Go 1.13 or later

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-wget https://github.com/golang/go/archive/go1.11.tar.gz && tar xf go1.11.tar.gz && cd go-go1.11 && patch -p1 < ../paddingmodes-go1.11.diff && cd src/ && ./make.bash && cd ../ && ./bin/go build -o ../padcheck ../padcheck.go
+wget https://github.com/golang/go/archive/go1.11.tar.gz && tar xf go1.11.tar.gz && cd go-go1.11 && patch -p1 < ../paddingmodes-go1.11.diff && cd src/ && GO111MODULE=auto ./make.bash && cd ../ && ./bin/go build -o ../padcheck ../padcheck.go
